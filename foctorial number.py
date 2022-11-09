4ca833c2f5e36976fdf4f30f2acdbe740a04caab
udhay86kumar <105726624+udhay86kumar@users.noreply.github.com>
2022-11-09 12:23:16 +0530

    delete

--- foctorial number.py
+++
@@ -1,8 +0,0 @@
-foc = int(input("Enter the factorial number:"))
-factorial = 1
-if foc <=0:
-    print("No factorial value for zero")
-else:
-    for i in range(1,foc+1):
-        factorial=factorial*i
-        print("value for the ",foc,"is",factorial)
